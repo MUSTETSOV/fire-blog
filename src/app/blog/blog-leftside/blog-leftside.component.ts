0a8e4020c3cbbe65344732c020370a65c1e82b81
unknown <tymofiimustetsov@gmail.com>
2019-10-11 23:42:15 +0300

    oct 11 v2

--- src/app/blog/blog-leftside/blog-leftside.component.ts
+++ src/app/blog/blog-leftside/blog-leftside.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../blog.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Blog } from '../blog';
+
 
 @Component({
   selector: 'app-blog-leftside',
@@ -9,14 +13,31 @@ import { BlogService } from '../blog.service';
 export class BlogLeftsideComponent implements OnInit {
 
 
-  blogs$;
+  blogs$: Observable<Blog[]>;
   categories$;
+  webBlogs$;
+
+
 
   constructor(blogService: BlogService) {
 
-    this.blogs$ = blogService.loadAllBlogs();
+   this.blogs$ = blogService.loadAllBlogs();
     this.categories$ = blogService.getCategories();
-   }
+
+    
+   
+    }
+
+    webBlogs() {
+      this.blogs$ = this.blogs$.pipe(
+        map(blogs => blogs.filter(
+            blog => blog.category.includes("Web design"))));
+        }
+
+
+
+
+
 
   ngOnInit() {
   }
