fb03be8d94de99ffd1ece9736805af521c9801e7
unknown <tymofiimustetsov@gmail.com>
2019-10-13 18:42:17 +0300

    okt 12 comments

--- src/app/blog/blog-details-leftside/blog-details-leftside.component.ts
+++ src/app/blog/blog-details-leftside/blog-details-leftside.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Blog } from '../blog';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-blog-details-leftside',
@@ -11,7 +14,27 @@ export class BlogDetailsLeftsideComponent implements OnInit {
 
   blog: Blog;
 
-  constructor(private route: ActivatedRoute) { }
+
+  formTemplate = new FormGroup({
+    caption: new FormControl(''),
+    date: new FormControl(''),
+    text: new FormControl('')
+  });
+
+
+
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) { }
+
+
+
+  onSubmit(formValue) {
+         //   this.resetForm();
+            this.firestore.collection('blogs').doc(this.blog.id).collection('comments').add(formValue);
+  }
+
+
+
+
 
   ngOnInit() {
 
